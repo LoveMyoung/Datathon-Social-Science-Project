142a2503ab65986bb53961819e41f6eb106837f8
lovemyoung <ssarang@ad.unc.edu>
2024-09-28 12:22:08 -0400

    I think I got the training and testing sets made

---
+++ dataCleaning.py
@@ -0,0 +1,18 @@
+from numpy.random import RandomState
+import pandas as pd
+
+
+#filename = "google_review_ratings.csv"
+
+
+df = pd.read_csv('google_review_ratings.csv')
+rng = RandomState()
+
+train = df.sample(frac=0.7, random_state=rng)
+test = df.loc[~df.index.isin(train.index)]
+
+print(test)
+print(train)
+
+train.to_csv("TrainingSet")
+test.to_csv("TestingSet")
